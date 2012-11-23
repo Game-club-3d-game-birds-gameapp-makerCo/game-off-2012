ba3dd7d3538ea404f273f10a90d5ee7e46c2dd14
SF <SF@Aarons-iMac-2.local>
2012-11-22 19:06:29 -0600

    Added files that were supposed to be in last commit.

--- helpervars.lua
+++ helpervars.lua
@@ -1,14 +1,3 @@
 --EVERYTHING IS A GLOBAL WAT
 
-walk_acceleration = 8
-walk_acceleration_air = 8
-min_speed = 0.7 -- Simulates static friction. If your speed falls below this you'll stop rather than slowly sliding
-max_walk_speed = 6.4
-friction = 14
-friction_air = 0
-speed_limiting_friction = friction * 2 -- Friction to apply when trying to move faster than max_walk_speed
-air_control_multiplier = 0.8 -- How much acceleration to apply when trying to move backward mid air
-
 gravity = 80
-jumping_gravity = 30 -- Weaker than actual gravity so jumping feels less like a jetpack
-jump_force = 16
--- petronius.lua
+++ petronius.lua
@@ -1,6 +1,20 @@
+require "class"
+
 petronius = class:new()
 
-function petronius:init(x, y)
+function petronius:init(x, y, is_player)
+
+	-- Helper variables
+	walk_acceleration = 8
+	walk_acceleration_air = 8
+	stopping_speed = 0.7 -- Simulates static friction. If your speed falls below this you'll stop rather than continuing to slide really slowly
+	max_walk_speed = 6.4
+	friction = 14
+	friction_air = 0
+	speed_limiting_friction = friction * 2 -- Friction to apply when trying to move faster than max_walk_speed
+	air_control_multiplier = 0.8 -- How much acceleration to apply when trying to move backward mid air
+	jumping_gravity = 30 -- Weaker than actual gravity so jumping feels less like a jetpack
+	jump_force = 16
 
 	-- Physical attributes
 	self.speedx = 0
@@ -9,6 +23,8 @@ function petronius:init(x, y)
 	self.width = 12/16
 	self.height = 12/16
 	self.y = y+1-self.height
+	is_player = is_player or false
+	is_jumping = false
 
 	-- Visual attributes
 
