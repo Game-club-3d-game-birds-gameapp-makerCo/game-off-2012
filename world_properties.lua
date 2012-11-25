670f9d7a7ba663e9a16484788e3dc3c80660e840
SF <SF@Aarons-iMac-2.local>
2012-11-25 15:15:39 -0600

    Reduced pixel scale to 2.

--- world_properties.lua
+++ world_properties.lua
@@ -1,5 +1,5 @@
 --EVERYTHING IS A GLOBAL WAT
 
 gravity = 80
-pixel_scale = 4
+pixel_scale = 2
 spawn_point = vector(love.graphics.getWidth() / (2 * pixel_scale), love.graphics.getHeight() / (2 * pixel_scale))
