d6970bca314f5bc161006abd6061c89c25da1d1c
Misty De Meo <mistydemeo@gmail.com>
2012-11-25 21:59:00 -0600

    Move print statements after scale

--- game.lua
+++ game.lua
@@ -18,14 +18,13 @@ function game:keyreleased(key, code)
 end
 
 function game:draw()
-    
-    love.graphics.print("X velocity: " .. player.velocity.x,10,10)
-    love.graphics.print("Player position: ( " .. player.pos.x .. ", " .. player.pos.y .. ")" ,10,30)
-    love.graphics.print("Facing right? " .. tostring(player.is_facing_right),10,50)
-    love.graphics.print("Movement state: " .. tostring(player.movement_state),10,70)
     love.graphics.push()
         love.graphics.scale(pixel_scale,pixel_scale) -- Pixels!
         player:draw()
         love.graphics.pop()
     love.graphics.rectangle("fill", 0, window_size.y / 2, window_size.x, window_size.y / 2)
+    love.graphics.print("X velocity: " .. player.velocity.x,10,10)
+    love.graphics.print("Player position: ( " .. player.pos.x .. ", " .. player.pos.y .. ")" ,10,30)
+    love.graphics.print("Facing right? " .. tostring(player.is_facing_right),10,50)
+    love.graphics.print("Movement state: " .. tostring(player.movement_state),10,70)
 end
